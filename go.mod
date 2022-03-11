e7485860a828ea9c0a01f3e64c5427bc90bc62c3
Nikolas North <nikolasn09@gmail.com>
2022-03-11 14:04:55 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/nikolasnorth/lamport-clocks
+
+go 1.17
+
+require (
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/consul/api v1.12.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.1.0 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220307174427-659dce7fcb03 // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ node/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/consul/api"
+	bank "github.com/nikolasnorth/lamport-clocks/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+const accountsFilename = "accounts.json"
+
+// Storage for all Account funds
+var accounts []Account
+
+type Account struct {
+	Name      *string  `json:"Name"`
+	AccountID *int64   `json:"AccountID"`
+	Balance   *float32 `json:"Balance"`
+}
+
+type Node struct {
+	Name    string
+	Addr    string
+	SDAddr  string
+	SDKV    api.KV
+	Clients map[string]bank.BankClient
+}
+
+type BankServer struct {
+	bank.UnimplementedBankServer
+}
+
+// Start gRPC server
+func (n *Node) listen() {
+	listener, err := net.Listen("tcp", n.Addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	bank.RegisterBankServer(server, &BankServer{})
+	reflection.Register(server)
+
+	err = server.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+}
+
+// Register this node with the service discovery module.
+func (n *Node) registerService() {
+	config := api.DefaultConfig()
+	config.Address = n.SDAddr
+
+	consul, err := api.NewClient(config)
+	if err != nil {
+		log.Fatalf("failed to contact service discovery: %v", err)
+	}
+
+	kv := consul.KV()
+	pair := &api.KVPair{Key: n.Name, Value: []byte(n.Addr)}
+
+	_, err = kv.Put(pair, nil)
+	if err != nil {
+		log.Fatalf("failed to register with service discovery: %v", err)
+	}
+
+	n.SDKV = *kv
+	log.Println("Successfully registered with service discovery.")
+}
+
+func (n *Node) setupClient(name, addr string) {
+	connection, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer connection.Close()
+
+	n.Clients[name] = bank.NewBankClient(connection)
+
+	// TODO: Perform RPC call
+}
+
+func (n *Node) greetAll() {
+	kvPairs, _, err := n.SDKV.List("nnorth2", nil)
+	if err != nil {
+		log.Fatalf("failed to retrieve list of all nodes: %v", err)
+	}
+
+	for _, pair := range kvPairs {
+		if pair.Key == n.Name {
+			continue
+		}
+		if n.Clients[pair.Key] == nil {
+			fmt.Println("New member: ", pair.Key)
+			n.setupClient(pair.Key, string(pair.Value))
+		}
+	}
+}
+
+func (n *Node) start() {
+	go n.listen()
+
+	n.registerService()
+
+	for {
+		time.Sleep(20 * time.Second)
+		n.greetAll()
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 3 {
+		log.Fatalln("Arguments required: [port number] [filename] [node name]")
+	}
+	name := args[0]
+	port := args[1]
+	addr := "localhost" + port
+	sdAddr := args[2]
+
+	accountsFile, err := os.Open(accountsFilename)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	bytes, err := io.ReadAll(accountsFile)
+	if err != nil {
+		log.Fatalf("failed to read from file: %v", err)
+	}
+
+	err = json.Unmarshal(bytes, &accounts)
+	if err != nil {
+		log.Fatalf("failed to unmarshal data into slice of accounts: %v", err)
+	}
+
+	n := Node{
+		Name:    name,
+		Addr:    addr,
+		SDAddr:  sdAddr,
+		SDKV:    api.KV{},
+		Clients: make(map[string]bank.BankClient),
+	}
+	n.start()
+}
